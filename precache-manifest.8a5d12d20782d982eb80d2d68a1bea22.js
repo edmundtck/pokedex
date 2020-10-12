ade0dfd28274d303c6d482bf27bf2983ed640c36
Edmund Teo <edmundteo.ck@gmail.com>
2020-10-12 16:21:09 +0800

    Updates

---
+++ precache-manifest.8a5d12d20782d982eb80d2d68a1bea22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3565cbcf04d852c6781e58429891a6b6",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "8993ed2043e855185e21",
+    "url": "/pokedex/static/css/main.aa2f871b.chunk.css"
+  },
+  {
+    "revision": "1203cbcd5b0983cb03c7",
+    "url": "/pokedex/static/js/2.c423f07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.c423f07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8993ed2043e855185e21",
+    "url": "/pokedex/static/js/main.2a58bac0.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
